42cb24425a71740480b309591366bfe97eae6de0
sko3on <tmdgns0311@naver.com>
2024-03-28 14:08:31 +0900

    뷰 바인딩 추가

--- app/src/main/java/com/example/mechuand/MainActivity.kt
+++ app/src/main/java/com/example/mechuand/MainActivity.kt
@@ -7,8 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 초기 테스트하는중 커밋테스트
-        // 도현호 커밋
-        // 12
+        
+
     }
 }
